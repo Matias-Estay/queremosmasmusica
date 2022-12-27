309814088096cae72a82c2a85533ae1e4e37c85e
Matias-Estay <matias.estay@hotmail.com>
2022-12-27 11:32:38 -0300

    formulario correo

--- src/components/ContactoTab.vue
+++ src/components/ContactoTab.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="row">
-    asdklñasdklñaskdñlakslñd
+  <div class="row justify-center">
+    <h3 class="q-mb-none">¿Tienes alguna duda?</h3>
+  </div>
+  <div class="row justify-center">
+    <h6 class="q-mb-none q-mt-none">Contacta con nosotros</h6>
+  </div>
+  <div class="separator"/>
+  <div class="row justify-center q-mt-lg">
+    <div class="col-xl-9">
+      <q-input square filled v-model="nombre" label="Tu nombre" />
+    </div>
   </div>
 </template>
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   setup() {
-
+    const nombre = ref('')
+    return{
+      nombre,
+    }
   },
 })
 </script>
